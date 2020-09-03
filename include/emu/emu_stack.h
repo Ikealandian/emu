f51e78d08f41c4ab55cd19d13e501837374b8095
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-04 01:54:24 +0200

    Create emu_stack.h

---
+++ include/emu/emu_stack.h
@@ -0,0 +1,34 @@
+// EMU
+//  Essential Multi-platform Utilities
+
+// emu/emu_stack.h
+#ifndef _EMU_STACK_H_
+#define _EMU_STACK_H_
+
+// emu_stack structure
+typedef struct emu_stack
+{
+    // Data
+    void** Data;
+    unsigned Size;
+    unsigned Capacity;
+
+    // Settings
+    unsigned ResizeAmount;
+    unsigned DefaultCapacity; 
+
+}emu_stack;
+
+// Initialize stack data
+void emu_alloc_stack(emu_stack* _stack);
+
+// Pop front element off stack
+void* emu_pop_stack(emu_stack* _stack);
+
+// Push to front
+void* emu_push_stack(emu_stack* _stack, void* _data);
+
+// Free stack data
+void emu_free_stack(emu_stack* _stack);
+
+#endif
