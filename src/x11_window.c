013d57082f44e07cdb4454ae4d706589e6098aa3
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-03 23:01:14 +0200

    Create x11_window.c

---
+++ src/x11_window.c
@@ -0,0 +1,37 @@
+// EMU
+//  Essential Multi-platform Utilities
+
+// X11 Window
+// src/x11_window.c
+#include <emu/emu_window.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <X11/X.h>
+
+typedef struct emu_window
+{
+    const char* wTitle;
+    long wWidth, wHeight;
+    long wFlags;
+
+}emu_window;
+
+emu_window* emu_create_window(const char* _title, long _width, long _height, long _flags)
+{
+    emu_window* eWindow = malloc(sizeof(emu_window));
+    eWindow->wTitle = _title;
+    eWindow->wWidth = _width;
+    eWindow->wHeight = _height;
+    eWindow->wFlags = _flags;
+
+    return eWindow;
+}
+
+void emu_destroy_window(emu_window* _window)
+{    
+    // free emu_window*
+    free(_window);
+}
