93b4b5e039a1bb4a76b2462bb530b7545f80c53c
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-04 01:55:11 +0200

    Remove void* return from emu_push_stack

--- include/emu/emu_stack.h
+++ include/emu/emu_stack.h
@@ -26,7 +26,7 @@ void emu_alloc_stack(emu_stack* _stack);
 void* emu_pop_stack(emu_stack* _stack);
 
 // Push to front
-void* emu_push_stack(emu_stack* _stack, void* _data);
+void emu_push_stack(emu_stack* _stack, void* _data);
 
 // Free stack data
 void emu_free_stack(emu_stack* _stack);
