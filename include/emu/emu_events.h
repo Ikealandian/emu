470520c5f631460c44ce1254985869b9b10aa206
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-04 01:14:51 +0200

    Create emu_events.h

---
+++ include/emu/emu_events.h
@@ -0,0 +1,23 @@
+// EMU
+//  Essential Multi-platform Utilities
+
+// emu/emu_events.h
+#ifndef _EMU_EVENTS_H_
+#define _EMU_EVENTS_H_
+
+// EMU Events
+
+// Window   0x0
+#define EMU_WINDOW_OPENED   0x01
+#define EMU_WINDOW_CLOSED   0x02
+
+// Debug    0x1
+#define EMU_DEBUG_MESSAGE   0x11
+
+// emu_event structure
+typedef struct emu_event
+{
+    long eType;
+}emu_event;
+
+#endif
