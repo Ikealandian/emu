9a90550acb4bbf73fd2e3adbd2cd9bb46847a9fc
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-04 01:02:30 +0200

    Poll window

--- examples/window_test.c
+++ examples/window_test.c
@@ -3,7 +3,16 @@
 int main()
 {
     emu_window* Window = emu_create_window("emu", 800L, 600L, 0L);
+
+    if (!Window)
+        return -1;
+
+    while (1)
+    {
+        emu_poll_window(Window);
+    }
+
     emu_destroy_window(Window);
-    
+
     return 0;
 }
