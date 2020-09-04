a77fdae6cc2470a244dbc8631ec3829063be128d
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-05 00:10:05 +0200

    rearange function declarations

--- include/emu/emu_stack.h
+++ include/emu/emu_stack.h
@@ -11,15 +11,12 @@ typedef struct emu_stack emu_stack;
 // Create emu_stack
 emu_stack* emu_create_stack(long _capacity, long _resize_by, long _type_size);
 
-
-
-// Pop front element off stack
-void* emu_pop_stack(emu_stack* _stack);
-
-// Push to front
+// Push element to front
 void emu_push_stack(emu_stack* _stack, void* _data);
 
-// Free stack data
+// Pop front element
+void* emu_pop_stack(emu_stack* _stack);
+
 // Destroy emu_stack
 void emu_destroy_stack(emu_stack* _stack);
 
