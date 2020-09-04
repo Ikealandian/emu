80be8ee620f028c110c2863e1c1f0d097d6145fd
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-05 01:22:46 +0200

    Implement emu_stack

--- src/emu_stack.c
+++ src/emu_stack.c
@@ -7,27 +7,33 @@
 
 #include <stdlib.h>
 
+
 typedef struct emu_stack
 {
-    void** sData;
-    long sTypeSize;
-    long sSize, sCapacity, sResize;
+    emu_event* sData;
+    long sSize, sCapacity;
 }emu_stack;
 
-int emu_resize_stack(emu_stack* _stack)
+#define EMU_STACK_RESIZE        5
+#define EMU_STACK_CAPACITY      24
+
+int emu_stack_resize(emu_stack* _stack)
 {
     // Increase capacity
-    _stack->sCapacity += _stack->sResize;
+    _stack->sCapacity += EMU_STACK_RESIZE;
 
     // Get new byte size
-    long sNewSize = _stack->sCapacity * _stack->sTypeSize;
+    long sNewSize = _stack->sCapacity * sizeof(emu_event);
 
     // Resize
-    void** sTmp = realloc(_stack->sData, sNewSize);
+    emu_event* sTmp = realloc(_stack->sData, sNewSize);
 
     // Check if resize was successful
     if (!sTmp)
+    {
+        free(_stack->sData);
         return 1;
+    }
 
     // Set stack data to resized data
     _stack->sData = sTmp;
@@ -35,22 +41,54 @@ int emu_resize_stack(emu_stack* _stack)
     return 0;
 }
 
-emu_stack* emu_create_stack(long _capacity, long _resize_by, long _type_size)
+emu_stack* emu_stack_create()
 {
+    // Allocate emu_stack
+    emu_stack* eStack = malloc(sizeof(emu_stack));
 
+    // Set values
+    eStack->sCapacity = EMU_STACK_CAPACITY;
+
+    // Allocate data
+    long sDataSize = eStack->sCapacity * sizeof(struct emu_event);
+    eStack->sData = malloc(sDataSize);
+
+    // Return the new stack
+    return eStack;
 }
 
-void emu_push_stack(emu_stack* _stack, void* _data)
+void emu_stack_clear(emu_stack* _stack)
 {
-
+    // Reset stack size
+    _stack->sSize = 0L;
 }
 
-void* emu_pop_stack(emu_stack* _stack)
+long emu_stack_size(emu_stack* _stack)
 {
-
+    return _stack->sSize;
 }
 
-void emu_destroy_stack(emu_stack* _stack)
+void emu_stack_push(emu_stack* _stack, emu_event _data)
 {
-    
+    if (_stack->sSize++ >= _stack->sCapacity)
+        emu_stack_resize(_stack);
+    _stack->sData[_stack->sSize - 1] = _data;
+}
+
+emu_event emu_stack_pop(emu_stack* _stack)
+{
+    // set size back so it can be overwritten
+    _stack->sSize--;
+    return _stack->sData[_stack->sSize];
+}
+
+void emu_stack_destroy(emu_stack* _stack)
+{
+    // Free stack data
+    free(_stack->sData);
+
+    // Free stack
+    free(_stack);
+
+    _stack = NULL;
 }
