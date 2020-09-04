d60c6e9ac562c67c90945568981c8fcd59fd8d59
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-05 00:10:54 +0200

    implement emu_stack resizing

--- src/emu_stack.c
+++ src/emu_stack.c
@@ -5,9 +5,31 @@
 // src/emu_stack.c
 #include <emu/emu_stack.h>
 
+#include <stdlib.h>
+
 typedef struct emu_stack
 {
     void** sData;
     long sTypeSize;
     long sSize, sCapacity, sResize;
 }emu_stack;
+int emu_resize_stack(emu_stack* _stack)
+{
+    // Increase capacity
+    _stack->sCapacity += _stack->sResize;
+
+    // Get new byte size
+    long sNewSize = _stack->sCapacity * _stack->sTypeSize;
+
+    // Resize
+    void** sTmp = realloc(_stack->sData, sNewSize);
+
+    // Check if resize was successful
+    if (!sTmp)
+        return 1;
+
+    // Set stack data to resized data
+    _stack->sData = sTmp;
+
+    return 0;
+}
