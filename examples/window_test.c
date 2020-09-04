636d780fd76f48a387bc3d1011a998f385958a98
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-05 01:22:27 +0200

    use emu_stack for events

--- examples/window_test.c
+++ examples/window_test.c
@@ -1,18 +1,41 @@
 #include <emu/emu_window.h>
+#include <emu/emu_stack.h>
+
+#include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
-    emu_window* Window = emu_create_window("emu", 800L, 600L, 0L);
+    emu_stack*  WindowEvents = emu_stack_create();
+    emu_window* Window = emu_window_create("emu", 800L, 600L, WindowEvents, 0L);
 
-    if (!Window)
-        return -1;
+    int Running = 1;
 
-    while (1)
+    while (Running)
     {
-        emu_poll_window(Window);
+        // Clear events and poll for new ones
+        emu_stack_clear(WindowEvents);
+        emu_window_poll(Window);
+
+        // Get Events
+        while (emu_stack_size(WindowEvents))
+        {
+            // Get First Event
+            emu_event Event = emu_stack_pop(WindowEvents);
+            switch (Event.Type)
+            {
+            case EMU_EVENT_WINDOW_CLOSED:
+            {
+                Running = 0;
+                puts("Window Closed");
+                break;
+            }
+            default: break;
+            }
+        }
     }
 
-    emu_destroy_window(Window);
-
+    emu_window_destroy(Window);
+    emu_stack_destroy(WindowEvents);
     return 0;
 }
