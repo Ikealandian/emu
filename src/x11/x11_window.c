6b7e513f921eccabccbba2f2117c40d527d68c92
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-05 01:23:20 +0200

    Poll for window closed event

--- src/x11/x11_window.c
+++ src/x11/x11_window.c
@@ -10,12 +10,14 @@
 
 #include <X11/X.h>
 #include <X11/Xlib.h>
+#include <X11/Xatom.h>
 
 typedef struct emu_window
 {
     const char* wTitle;
     long wX, wY;
     long wWidth, wHeight;
+    emu_stack* wEventStack;
     long wFlags;
 
     // X11
@@ -26,7 +28,22 @@ typedef struct emu_window
     Window xWindow;
     int nScreen;
 
+    // X11 Atoms
+    Atom WMDeleteWindow;
+
 }emu_window;
+
+void x11_get_atoms(emu_window* _window)
+{
+    _window->WMDeleteWindow = XInternAtom(_window->xDisplay, "WM_DELETE_WINDOW", False);
+
+    Atom WMProtocols[1] = 
+    {
+        _window->WMDeleteWindow
+    };
+
+    XSetWMProtocols(_window->xDisplay, _window->xWindow, WMProtocols, 1);
+}
 
 int x11_assemble_window(emu_window* _window)
 {
@@ -66,6 +83,9 @@ int x11_assemble_window(emu_window* _window)
     XStoreName(_window->xDisplay, _window->xWindow, _window->wTitle);
     XSetIconName(_window->xDisplay, _window->xWindow, _window->wTitle);
 
+    // Get X11 Atoms
+    x11_get_atoms(_window);
+
     // Select Event Masks
     XSelectInput(
         _window->xDisplay, _window->xWindow,
@@ -89,32 +109,54 @@ int x11_assemble_window(emu_window* _window)
     return 0;
 }
 
-emu_window* emu_create_window(const char* _title, long _width, long _height, long _flags)
+emu_window* emu_window_create(const char* _title, long _width, long _height, emu_stack* _event_stack, long _flags)
 {
     emu_window* eWindow = malloc(sizeof(emu_window));
     eWindow->wTitle = _title;
     eWindow->wWidth = _width;
     eWindow->wHeight = _height;
+    eWindow->wEventStack = _event_stack;
     eWindow->wFlags = _flags;
 
     if (x11_assemble_window(eWindow) != 0)
     {
-        emu_destroy_window(eWindow);
+        emu_window_destroy(eWindow);
         return NULL;
     }
 
     return eWindow;
 }
 
-void emu_poll_window(emu_window* _window)
+void emu_window_poll(emu_window* _window)
 {
+    if (!_window->wEventStack)
+    {
+        fputs("EMU [X11] Window: Could not poll events (wEventStack == null)\n", stderr);
+        return;
+    }
+
     while (XPending(_window->xDisplay))
     {
         XNextEvent(_window->xDisplay, &_window->xEvent);
+
+        // X11 Client Message Event
+        if (_window->xEvent.type == ClientMessage)
+        {
+            XClientMessageEvent* Message = &_window->xEvent.xclient;
+
+            // Is window closed event?
+            if ((Atom)Message->data.l[0] == _window->WMDeleteWindow)
+            {
+                // push message to stack
+                emu_event emuEventWindowClosed;
+                emuEventWindowClosed.Type = EMU_EVENT_WINDOW_CLOSED;
+                emu_stack_push(_window->wEventStack, emuEventWindowClosed);
+            }
+        }
     }
 }
 
-void emu_destroy_window(emu_window* _window)
+void emu_window_destroy(emu_window* _window)
 {    
     // Destroy Window and Close Display
     XDestroyWindow(_window->xDisplay, _window->xWindow);
