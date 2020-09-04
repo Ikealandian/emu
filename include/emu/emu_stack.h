2545444d6bd7859befa6b6ff0aa8cf0d7bd5b4a3
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-05 01:21:58 +0200

    change naming scheme

--- include/emu/emu_stack.h
+++ include/emu/emu_stack.h
@@ -1,23 +1,32 @@
 // EMU
 //  Essential Multi-platform Utilities
 
+// Stack for emu events
 // emu/emu_stack.h
 #ifndef _EMU_STACK_H_
 #define _EMU_STACK_H_
+
+#include <emu/emu_events.h>
 
 // emu_stack
 typedef struct emu_stack emu_stack;
 
 // Create emu_stack
-emu_stack* emu_create_stack(long _capacity, long _resize_by, long _type_size);
+emu_stack* emu_stack_create();
+
+// Clear stack
+void emu_stack_clear(emu_stack* _stack);
+
+// Stack size
+long emu_stack_size(emu_stack* _stack);
 
 // Push element to front
-void emu_push_stack(emu_stack* _stack, void* _data);
+void emu_stack_push(emu_stack* _stack, emu_event _data);
 
 // Pop front element
-void* emu_pop_stack(emu_stack* _stack);
+emu_event emu_stack_pop(emu_stack* _stack);
 
 // Destroy emu_stack
-void emu_destroy_stack(emu_stack* _stack);
+void emu_stack_destroy(emu_stack* _stack);
 
 #endif
--- include/emu/emu_window.h
+++ include/emu/emu_window.h
@@ -5,6 +5,9 @@
 #ifndef _EMU_WINDOW_H_
 #define _EMU_WINDOW_H_
 
+// EMU Event Stack
+#include <emu/emu_stack.h>
+
 // EMU Window Flags
 #define EMU_WINDOW_CENTERED     (1 << 0)
 #define EMU_WINDOW_NORESIZE     (1 << 1)
@@ -22,16 +25,17 @@ typedef struct emu_screen
 }emu_screen;
 
 // Create emu_window
-emu_window* emu_create_window(
+emu_window* emu_window_create(
     const char* _title,
     long _width, long _height,
+    emu_stack* _event_stack,
     long _flags
 );
 
 // Poll emu_window Events
-void emu_poll_window(emu_window* _window);
+void emu_window_poll(emu_window* _window);
 
 // Destroy emu_window
-void emu_destroy_window(emu_window* _window);
+void emu_window_destroy(emu_window* _window);
 
 #endif
