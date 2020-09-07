469c49a049fe36d67e02c43946c148ed205a2a54
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-07 21:37:32 +0200

    Create emu_input.h

---
+++ include/emu/emu_input.h
@@ -0,0 +1,38 @@
+// EMU
+//  Essential Multi-platform Utilities
+
+// emu/emu_input.h
+#ifndef _EMU_INPUT_H_
+#define _EMU_INPUT_H_
+
+// EMU Input codes
+
+//      Key
+#define EMU_KEY_Q   0x11
+#define EMU_KEY_W   0x12
+#define EMU_KEY_E   0x13
+#define EMU_KEY_R   0x14
+#define EMU_KEY_T   0x15
+#define EMU_KEY_Y   0x16
+#define EMU_KEY_U   0x17
+#define EMU_KEY_I   0x18
+#define EMU_KEY_O   0x19
+#define EMU_KEY_P   0x1a
+#define EMU_KEY_A   0x1b
+#define EMU_KEY_S   0x1c
+#define EMU_KEY_D   0x1d
+#define EMU_KEY_F   0x1e
+#define EMU_KEY_G   0x1f
+#define EMU_KEY_H   0x21
+#define EMU_KEY_J   0x22
+#define EMU_KEY_K   0x23
+#define EMU_KEY_L   0x24
+#define EMU_KEY_Z   0x25
+#define EMU_KEY_X   0x26
+#define EMU_KEY_C   0x27
+#define EMU_KEY_V   0x28
+#define EMU_KEY_B   0x29
+#define EMU_KEY_N   0x2a
+#define EMU_KEY_M   0x2b
+
+#endif
