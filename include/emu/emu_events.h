f31c573b21a575dda9f3aaebc6e0e87bb06dfebe
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-07 21:37:50 +0200

    Add Key and Button events

--- include/emu/emu_events.h
+++ include/emu/emu_events.h
@@ -7,12 +7,39 @@
 
 // EMU Events
 
-// Window   0x0
-#define EMU_EVENT_WINDOW_OPENED   0x01
-#define EMU_EVENT_WINDOW_CLOSED   0x02
+// Window   0x1
+#define EMU_EVENT_WINDOW_OPENED     0x11
+#define EMU_EVENT_WINDOW_CLOSED     0x12
 
-// Debug    0x1
-#define EMU_EVENT_DEBUG_MESSAGE   0x11
+// Input    0x2
+#define EMU_EVENT_INPUT_KEY         0x21    // Key Event
+#define EMU_EVENT_INPUT_BUTTON      0x21    // Mouse Button Event
+#define EMU_EVENT_INPUT_SCROLL      0x21    // Scroll Wheel Event
+#define EMU_EVENT_INPUT_MOVED       0x21    // Pointer Moved Event
+
+// Debug    0x3
+#define EMU_EVENT_DEBUG_MESSAGE     0x31
+
+// emu_ekey
+typedef struct emu_ekey
+{
+    long Button, Action;
+    long Repeat;
+}emu_ekey;
+
+// emu_ebutton
+typedef struct emu_ebutton
+{
+    long Button, Action;
+    long PointerX, PointerY;
+}emu_ebutton;
+
+// emu_edbgmsg 
+typedef struct emu_dbgmsg
+{
+    long Type;
+    char* Message;
+}emu_edbgmsg;
 
 // emu_event structure
 typedef struct emu_event
@@ -20,14 +47,18 @@ typedef struct emu_event
     long Type;
 
     union
-    {
-        // Debug Message Event
-        struct
-        {
-            long Type;
-            char* Message;
-        }DebugMessage;
-        
+    {    
+        // EMU_EVENT_WINDOW_OPENED 
+        // EMU_EVENT_WINDOW_CLOSED
+        //  No Event Data
+
+        // EMU_EVENT_INPUT_KEY
+        // EMU_EVENT_INPUT_BUTTON
+        emu_ekey        uKeyEvent;
+        emu_ebutton     uButtonEvent;
+
+        // EMU_EVENT_DEBUG_MESSAGE
+        emu_edbgmsg     uDebugMessage;
     };
 
 }emu_event;
