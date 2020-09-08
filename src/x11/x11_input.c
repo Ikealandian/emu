f1b26159de0f75e71050bdd0d854d728f573e922
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-08 23:58:54 +0200

    Create x11_input.c and add function outlines

---
+++ src/x11/x11_input.c
@@ -0,0 +1,41 @@
+// EMU
+//  Essential Multi-platform Utilities
+
+// X11 Input
+// src/x11/x11_input.c
+#include "x11_platform.h"
+
+void x11_handle_char(emu_window* _window)
+{
+
+}
+
+void x11_handle_key_press(emu_window* _window)
+{
+
+}
+
+void x11_handle_key_release(emu_window* _window)
+{
+
+}
+
+void x11_handle_button_press(emu_window* _window)
+{
+
+}
+
+void x11_handle_button_release(emu_window* _window)
+{
+
+}
+
+void x11_handle_scroll(emu_window* _window)
+{
+
+}
+
+void x11_handle_moved(emu_window* _window)
+{
+
+}
